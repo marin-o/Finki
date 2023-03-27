e93246fd0c2bce07c063aa89014c25a5dc5d3fb5
Marino <107618043+M-Jak@users.noreply.github.com>
2023-03-27 17:54:48 +0200

    fixed countrheads to meet criteria

--- Operativni Sistemi/Lab1_B/Zadaca4/CountThreads.java
+++ Operativni Sistemi/Lab1_B/Zadaca4/CountThreads.java
@@ -3,8 +3,8 @@ public class CountThreads {
 
 
 public static void main(String[] args) throws InterruptedException {
-    Thread t1 = new Thread(new ThreadPair('A'));
-    Thread t2 = new Thread(new ThreadPair('0'));
+    Thread t1 = new Thread(new ThreadPair(new ArrayList<Character>(Arrays.asList('A','B','C','D','E','F','G','H','I','J'));
+    Thread t2 = new Thread(new ThreadPair(new ArrayList<Character>(Arrays.asList('1','2','3','4','5','6','7','8','9','10'));
     t1.start();
     t1.join();
     t2.start();
@@ -14,16 +14,16 @@ public static void main(String[] args) throws InterruptedException {
 }
 
 class ThreadPair implements Runnable{
-    char type;
+    ArrayList<Character> chars
 
-    public ThreadPair(char type){
-        this.type=type;
+    public ThreadPair(ArrayList<Character> chars){
+        this.chars=chars;
     }
 
     @Override
     public void run(){
         for(int i=0;i<10;i++){
-            System.out.println(type+i);
+            System.out.println(chars.get(i));
         }
     }
 }
