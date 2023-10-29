12f5d9453e4568b5713a73a3e842e3c06d56b877
Marino <marinoj.business@gmail.com>
2023-10-29 22:50:26 +0100

    added real movies to MovieRepository.java init

--- Web programiranje/lab_1/lab1A/src/main/java/mk/finki/ukim/mk/lab/repository/MovieRepository.java
+++ Web programiranje/lab_1/lab1A/src/main/java/mk/finki/ukim/mk/lab/repository/MovieRepository.java
@@ -5,6 +5,7 @@ import mk.finki.ukim.mk.lab.model.Movie;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,14 +15,17 @@ public class MovieRepository {
 
     @PostConstruct
     private void init(){
-        java.util.Random random = new java.util.Random();
-        for ( int i=0; i < 10; i++ ) {
-            movies.add(
-                    new Movie("Movie " + i,
-                            "Summary for movie " + i,
-                            random.nextDouble(0,100)
-                    ));
-        }
+        this.movies = Arrays.asList(
+                new Movie("The Shawshank Redemption", "Two imprisoned men bond over several years, finding solace and redemption.", 9.3),
+                new Movie("The Godfather", "The aging patriarch of a crime dynasty transfers control to his reluctant son.", 9.2),
+                new Movie("The Dark Knight", "Batman faces the Joker's havoc and chaos in Gotham.", 9.0),
+                new Movie("Schindler's List", "Industrialist Oskar Schindler saves Jewish workers during World War II.", 8.9),
+                new Movie("Pulp Fiction", "Intersecting stories of mob hitmen, a boxer, and two diner bandits.", 8.9),
+                new Movie("The Lord of the Rings", "Gandalf and Aragorn lead the fight against Sauron's army.", 8.9),
+                new Movie("Forrest Gump", "An Alabama man experiences historical events from the 1950s to the 1980s.", 8.8),
+                new Movie("Inception", "A thief plants an idea into a CEO's mind.", 8.7),
+                new Movie("The Matrix", "A computer hacker fights against controllers of his reality.", 8.7),
+                new Movie("Titanic", "A love story on the ill-fated R.M.S. Titanic.", 7.8));
     }
 
     public List<Movie> findAll(){
