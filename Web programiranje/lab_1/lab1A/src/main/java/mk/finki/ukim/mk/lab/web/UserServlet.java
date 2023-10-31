0e5ad1f73c482165265893c3504550e8b46fce36
211017 <marinoj.business@gmail.com>
2023-10-31 17:55:20 +0100

    adasd

---
+++ Web programiranje/lab_1/lab1A/src/main/java/mk/finki/ukim/mk/lab/web/UserServlet.java
@@ -0,0 +1,37 @@
+package mk.finki.ukim.mk.lab.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import mk.finki.ukim.mk.lab.service.impl.UserServiceImpl;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/users")
+@AllArgsConstructor
+public class UserServlet extends HttpServlet {
+    private final UserServiceImpl userService;
+    private final SpringTemplateEngine templateEngine;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        IWebExchange webExchange = JakartaServletWebApplication
+                .buildApplication(getServletContext())
+                .buildExchange(req,resp);
+        WebContext context = new WebContext(webExchange);
+
+        templateEngine.process("userHistory.html",context,resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
