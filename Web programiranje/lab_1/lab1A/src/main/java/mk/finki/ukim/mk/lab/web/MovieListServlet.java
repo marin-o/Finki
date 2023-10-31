853b7cef3ed2c50024f8d26f39010d95b0395f45
Marino <marinoj.business@gmail.com>
2023-10-31 15:25:11 +0100

    replaced constructors with annotation. memorypuzzle question 3.py no idea what i changed xd

--- Web programiranje/lab_1/lab1A/src/main/java/mk/finki/ukim/mk/lab/web/MovieListServlet.java
+++ Web programiranje/lab_1/lab1A/src/main/java/mk/finki/ukim/mk/lab/web/MovieListServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import mk.finki.ukim.mk.lab.service.impl.MovieServiceImpl;
 import mk.finki.ukim.mk.lab.service.impl.TicketOrderServiceImpl;
 import org.thymeleaf.context.WebContext;
@@ -14,17 +15,10 @@ import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 import java.io.IOException;
 
 @WebServlet(urlPatterns="")
+@AllArgsConstructor
 public class MovieListServlet extends HttpServlet {
     private final MovieServiceImpl movieService;
-    private final TicketOrderServiceImpl ticketOrderService;
     private final SpringTemplateEngine templateEngine;
-
-    public MovieListServlet( MovieServiceImpl movieService, TicketOrderServiceImpl ticketOrderService,
-                             SpringTemplateEngine templateEngine) {
-        this.movieService=movieService;
-        this.ticketOrderService = ticketOrderService;
-        this.templateEngine=templateEngine;
-    }
 
     @Override
     protected void doGet( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
