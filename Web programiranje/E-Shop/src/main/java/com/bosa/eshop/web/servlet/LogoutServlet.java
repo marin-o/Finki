679a4c7b703269ea4f74468c11397cb0af447a7a
Marino <marinoj.business@gmail.com>
2023-11-05 21:43:04 +0100

    idk sho bea promenive

---
+++ Web programiranje/E-Shop/src/main/java/com/bosa/eshop/web/servlet/LogoutServlet.java
@@ -0,0 +1,18 @@
+package com.bosa.eshop.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns="/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect("/");
+    }
+}
