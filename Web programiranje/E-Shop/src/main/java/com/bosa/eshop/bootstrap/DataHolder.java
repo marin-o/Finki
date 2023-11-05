5e77f031774158e396594d06fd3143eedf7a5bb2
Marino <marinoj.business@gmail.com>
2023-11-05 20:24:35 +0100

    modified dataholder and created InMemoryUserRepo

--- Web programiranje/E-Shop/src/main/java/com/bosa/eshop/bootstrap/DataHolder.java
+++ Web programiranje/E-Shop/src/main/java/com/bosa/eshop/bootstrap/DataHolder.java
@@ -1,6 +1,7 @@
 package com.bosa.eshop.bootstrap;
 
 import com.bosa.eshop.model.Category;
+import com.bosa.eshop.model.User;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +11,14 @@ import java.util.List;
 @Component
 public class DataHolder {
     public static List<Category> categories = new ArrayList<>();
+    public static List<User> users = new ArrayList<>();
 
     @PostConstruct
     public void init(){
         categories.add(new Category("Softver","so"));
         categories.add(new Category("Books","bo"));
+
+        users.add(new User("marino", "mj", "Marino","Jakimoski"));
+        users.add(new User("farbano", "fb", "Farbano","Bosa"));
     }
 }
