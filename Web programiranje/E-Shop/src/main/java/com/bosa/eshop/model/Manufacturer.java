e9c50907534fc0965d44917fa3acfc54cf3bf1df
Marino <marinoj.business@gmail.com>
2023-12-03 17:41:44 +0100

    vezba 6 jpa done ama idk dal rabotat poso riste praese na snimkana so postgresql baza a mene ne mi rabotat ovde i maka mi e da setupvam

--- Web programiranje/E-Shop/src/main/java/com/bosa/eshop/model/Manufacturer.java
+++ Web programiranje/E-Shop/src/main/java/com/bosa/eshop/model/Manufacturer.java
@@ -1,16 +1,23 @@
 package com.bosa.eshop.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@NoArgsConstructor
 public class Manufacturer {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String address;
 
     public Manufacturer( String name, String address ) {
-        this.id = (long) (Math.random() * 1000);
         this.name=name;
         this.address=address;
     }
--- Web programiranje/E-Shop/src/main/java/com/bosa/eshop/model/ShoppingCart.java
+++ Web programiranje/E-Shop/src/main/java/com/bosa/eshop/model/ShoppingCart.java
@@ -1,26 +1,31 @@
 package com.bosa.eshop.model;
 
 import com.bosa.eshop.model.enumeration.ShoppingCartStatus;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Entity
+@NoArgsConstructor
 public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime dateCreated;
+    @ManyToOne
     private User user;
+    @ManyToMany
     private List<Product> products;
+    @Enumerated(EnumType.STRING)
     private ShoppingCartStatus status;
 
-    public ShoppingCart() {
-        this.id = (long) (Math.random()*1000);
-    }
 
     public ShoppingCart(User user) {
-        this.id = (long) (Math.random()*1000);
         this.dateCreated = LocalDateTime.now();
         this.user = user;
         this.products = new ArrayList<>();
