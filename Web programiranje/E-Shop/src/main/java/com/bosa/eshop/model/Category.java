f412839df5d56a697f8bcc99e4ad744fb4f21601
Marino <marinoj.business@gmail.com>
2023-12-03 17:41:44 +0100

    vezba 6 jpa done ama idk dal rabotat poso riste praese na snimkana so postgresql baza a mene ne mi rabotat ovde i maka mi e da setupvam

--- Web programiranje/E-Shop/src/main/java/com/bosa/eshop/model/Category.java
+++ Web programiranje/E-Shop/src/main/java/com/bosa/eshop/model/Category.java
@@ -1,16 +1,24 @@
 package com.bosa.eshop.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@NoArgsConstructor
 public class Category{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(length = 4000)
     private String description;
 
     public Category( String name, String description ) {
-        this.id = (long) (Math.random() * 1000);
         this.name=name;
         this.description=description;
     }
+
+
 }
--- Web programiranje/E-Shop/src/main/java/com/bosa/eshop/model/Product.java
+++ Web programiranje/E-Shop/src/main/java/com/bosa/eshop/model/Product.java
@@ -1,18 +1,25 @@
 package com.bosa.eshop.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Double price;
     private Integer quantity;
+    @ManyToOne
     private Category category;
+    @ManyToOne
     private Manufacturer manufacturer;
 
     public Product( String name, Double price, Integer quantity, Category category, Manufacturer manufacturer ) {
-        this.id = (long) (Math.random() * 1000);
         this.name=name;
         this.price=price;
         this.quantity=quantity;
