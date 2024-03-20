f1589f4783e136ead9979f4cc69fca4eb58dab9c
Marino <marinoj.business@gmail.com>
2024-03-20 01:43:57 +0100

    lab 1 is

---
+++ Integrirani Sistemi/Lab1/Lab1.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using Lab1.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab1.Web.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Integrirani Sistemi/Lab1/Lab1.Web/Models/Domain/TheaterShow.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Web.Models.Domain {
+    public class TheaterShow {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime ShowDate { get; set; }
+        public string? Place { get; set; }
+        public string? ImageUrl { get; set; }
+        public virtual ICollection<Ticket>? Tickets { get; set; }
+    }
+}
