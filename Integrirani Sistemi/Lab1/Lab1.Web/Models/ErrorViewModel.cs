ca723e11494b83444c06d3334092913488b726a3
Marino <marinoj.business@gmail.com>
2024-04-01 22:43:07 +0200

    a

--- Integrirani Sistemi/Lab1/Lab1.Web/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace Lab1.Web.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
