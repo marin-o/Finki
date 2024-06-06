b3c480e2b3f9cc801be5cf91f5156c41ee7e6f2a
M-Jak <marinoj.business@gmail.com>
2024-06-06 22:18:27 +0200

    доне

--- Dizajn na interakcijata covek kompjuter/kolokviumski/nekoja_zadaca/zadaca/forms.py
+++ Dizajn na interakcijata covek kompjuter/kolokviumski/nekoja_zadaca/zadaca/forms.py
@@ -8,7 +8,9 @@ class EventForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(EventForm, self).__init__(*args, **kwargs)
         for field in self.visible_fields():
+            # додавање бутстрап во форма
             field.field.widget.attrs["class"] = "form-control"
+            # додавање чекбокс бутстрап во форма
             if field.name == 'open_air':
                 field.field.widget.attrs["class"] = "form-check-input"
     class Meta:
