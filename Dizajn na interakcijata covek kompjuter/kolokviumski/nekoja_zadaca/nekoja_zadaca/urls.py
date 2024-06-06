8c2950ede46a392e612b32d46af3df0ac1136205
M-Jak <marinoj.business@gmail.com>
2024-06-06 23:02:45 +0200

    доне

--- Dizajn na interakcijata covek kompjuter/kolokviumski/nekoja_zadaca/nekoja_zadaca/urls.py
+++ Dizajn na interakcijata covek kompjuter/kolokviumski/nekoja_zadaca/nekoja_zadaca/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index, name='index'),
     path('newevent/', newevent, name='newevent'),
+    path('editevent/<int:event_id>', editevent, name='editevent'),
+    path('deleteevent/<int:event_id>', deleteevent, name='deleteevent'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Dizajn na interakcijata covek kompjuter/kolokviumski/nekoja_zadaca/zadaca/views.py
+++ Dizajn na interakcijata covek kompjuter/kolokviumski/nekoja_zadaca/zadaca/views.py
@@ -37,3 +37,30 @@ def newevent(request):
         event_form = EventForm()
     context = {'form': event_form}
     return render(request, 'newevent.html', context)
+
+
+def editevent(request, event_id):
+    instance = Event.objects.filter(id=event_id).get()
+    if request.method == 'POST':
+        event_form = EventForm(request.POST, instance=instance)
+        if event_form.is_valid():
+            event = event_form.save(commit=False)
+            event.save()
+            participants = event_form.cleaned_data['participants'].split(',')
+            for participant in participants:
+                band = Band.objects.filter(name=participant).first()
+                if band:
+                    if not BandEvent.objects.filter(event=instance).exists():
+                        BandEvent.objects.create(band=band, event=event)
+                    for bandevent in BandEvent.objects.filter(event=instance).all():
+                        if bandevent.band.name not in participants:
+                            bandevent.delete()
+            return redirect(to='index')
+    else:
+        event_form = EventForm(instance=instance)
+    return render(request, "newevent.html", {'form': event_form})
+
+
+def deleteevent(request, event_id):
+        Event.objects.filter(id=event_id).delete()
+        return redirect(to='index')
