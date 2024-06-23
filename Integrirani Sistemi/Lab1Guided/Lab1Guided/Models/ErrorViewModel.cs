7daebe11924e67178d6fcac862728955ee5262f3
M-Jak <marinoj.business@gmail.com>
2024-06-23 19:04:45 +0200

    message:

--- Integrirani Sistemi/Lab1Guided/Lab1Guided/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab1Guided.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
