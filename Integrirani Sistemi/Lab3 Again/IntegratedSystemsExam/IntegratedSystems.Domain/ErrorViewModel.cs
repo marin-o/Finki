691e1896b530ebf0f675e68bc95a892a10ae6c84
M-Jak <marinoj.business@gmail.com>
2024-06-23 19:03:12 +0200

    message

--- Integrirani Sistemi/Lab3 Again/IntegratedSystemsExam/IntegratedSystems.Domain/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IntegratedSystems.Domain
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
